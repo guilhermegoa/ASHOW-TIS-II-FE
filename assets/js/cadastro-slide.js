ebcfe7b07c6692f7731bc4ae3dc19dcfa98bb033
Guilherme <guilhergoa@hotmail.com>
2019-10-18 23:02:37 -0300

    v1

--- assets/js/cadastro-slide.js
+++ assets/js/cadastro-slide.js
@@ -1,18 +1,22 @@
 const slidemusico = () => {
    const btn_musico = document.getElementById('btn-musico');
    const form_musico = document.getElementById('form-musico');
+   const tela_musico = document.getElementById('musico');
 
    btn_musico.addEventListener('click', () => {
-      form_musico.classList.toggle('form-musico');
+      form_musico.classList.toggle('form-active');
+      tela_musico.classList.toggle('musico-active');
    });
 };
 
 const slidecontratante = () => {
    const btn_musico = document.getElementById('btn-contratante');
    const form_musico = document.getElementById('form-contratante');
+   const tela_contratante = document.getElementById('contratante');
 
    btn_musico.addEventListener('click', () => {
-      form_musico.classList.toggle('form-contratante');
+      form_musico.classList.toggle('form-active');
+      tela_contratante.classList.toggle('contratante-active');
    });
 };
 
