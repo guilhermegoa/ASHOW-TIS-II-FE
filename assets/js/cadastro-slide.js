fc7f77c2a5e9fa9450e795eca51d431429659aed
Guilherme <guilhergoa@hotmail.com>
2019-10-20 01:00:35 -0300

    fix gap top

--- assets/js/cadastro-slide.js
+++ assets/js/cadastro-slide.js
@@ -2,21 +2,29 @@ const slidemusico = () => {
    const btn_musico = document.getElementById('btn-musico');
    const form_musico = document.getElementById('form-musico');
    const tela_musico = document.getElementById('musico');
+   const tela_contratante = document.getElementById('contratante');
+   const aux = document.getElementById('div-contratante');
 
    btn_musico.addEventListener('click', () => {
       form_musico.classList.toggle('form-active');
       tela_musico.classList.toggle('musico-active');
+      tela_contratante.classList.toggle('contratante-active');
+      aux.classList.toggle('off');
    });
 };
 
 const slidecontratante = () => {
-   const btn_musico = document.getElementById('btn-contratante');
-   const form_musico = document.getElementById('form-contratante');
+   const btn_contratante = document.getElementById('btn-contratante');
+   const form_contratante = document.getElementById('form-contratante');
    const tela_contratante = document.getElementById('contratante');
+   const tela_musico = document.getElementById('musico');
+   const aux = document.getElementById('div-musico');
 
-   btn_musico.addEventListener('click', () => {
-      form_musico.classList.toggle('form-active');
+   btn_contratante.addEventListener('click', () => {
+      form_contratante.classList.toggle('form-active');
       tela_contratante.classList.toggle('contratante-active');
+      tela_musico.classList.toggle('musico-active');
+      aux.classList.toggle('off');
    });
 };
 
