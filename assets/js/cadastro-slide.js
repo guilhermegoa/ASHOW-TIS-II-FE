5776cef1f25e541d44cb0fbdfa7a13879483285c
Guilherme <guilhergoa@hotmail.com>
2019-10-21 18:12:11 -0300

    muda testo ao clicar

--- assets/js/cadastro-slide.js
+++ assets/js/cadastro-slide.js
@@ -4,12 +4,21 @@ const slidemusico = () => {
    const tela_musico = document.getElementById('musico');
    const tela_contratante = document.getElementById('contratante');
    const aux = document.getElementById('div-contratante');
+   var troca = new Boolean(true);
 
    btn_musico.addEventListener('click', () => {
       form_musico.classList.toggle('form-active');
       tela_musico.classList.toggle('musico-active');
       tela_contratante.classList.toggle('contratante-active');
       aux.classList.toggle('off');
+
+      if (troca) {
+         btn_musico.textContent = `Clique para cancelar`;
+         troca = false;
+      } else {
+         btn_musico.textContent = `Cadastrar como Músico`;
+         troca = true;
+      }
    });
 };
 
@@ -19,12 +28,21 @@ const slidecontratante = () => {
    const tela_contratante = document.getElementById('contratante');
    const tela_musico = document.getElementById('musico');
    const aux = document.getElementById('div-musico');
+   var troca = new Boolean(true);
 
    btn_contratante.addEventListener('click', () => {
       form_contratante.classList.toggle('form-active');
       tela_contratante.classList.toggle('contratante-active');
       tela_musico.classList.toggle('musico-active');
       aux.classList.toggle('off');
+
+      if (troca) {
+         btn_contratante.textContent = `Clique para cancelar`;
+         troca = false;
+      } else {
+         btn_contratante.textContent = `Cadastrar como Contratante`;
+         troca = true;
+      }
    });
 };
 
