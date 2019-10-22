53680ef5f98c1e3e62d59593532704dd0cca2757
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2019-10-22 19:19:07 -0300

    Checkpoint tela de cadastro de eventos (incompleta)

---
+++ assets/js/CadastroEvento.js
@@ -0,0 +1,20 @@
+const cadastroEventoInit = () => {
+   const formEvent = document.getElementById('formEvent');
+   const name = document.getElementById('name');
+   const eventType = document.getElementById('eventType');
+   const date = document.getElementById('date');
+   const CEP = document.getElementById('CEP');
+   const street = document.getElementById('street');
+   const number = document.getElementById('number');
+   const district = document.getElementById('district');
+   const city = document.getElementById('city');
+   const state = document.getElementById('state');
+   const complement = document.getElementById('complement');
+   const peopleQnt = document.getElementById('peopleQnt');
+   const artistValue = document.getElementById('artistValue');
+   const cadastrarEventoBtn = document.getElementById('cadastrarEventoBtn');
+
+   cadastrarEventoBtn.addEventListener('click', () => {
+      formEvent.ver;
+   });
+};
