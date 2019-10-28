258b42e748f78480801087c4ec8c517996f8105f
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2019-10-28 01:47:48 -0300

    nem sei o q fiz/ validação

--- assets/js/CadastroEvento.js
+++ assets/js/CadastroEvento.js
@@ -1,5 +1,5 @@
 const InitCadastroEvento = () => {
-   const form = $('#formEvent');
+   const form = $("#formEvent");
    const name = $("#name");
    const eventType = $("#eventType");
    const date = $("#date");
@@ -16,25 +16,25 @@ const InitCadastroEvento = () => {
 
    CEP.change(() => {
       if (CEP.val().length >= 8) {
-         $.getJSON("https://viacep.com.br/ws/" + CEP.val() + "/json/", function (dados) {
+         $.getJSON("https://viacep.com.br/ws/" + CEP.val() + "/json/", function(dados) {
             street.prop("disabled", false);
             district.prop("disabled", false);
             city.prop("disabled", false);
             state.prop("disabled", false);
             if (!("erro" in dados)) {
-               if (dados.logradouro != '') {
+               if (dados.logradouro != "") {
                   street.val(dados.logradouro);
                   street.prop("disabled", true);
                }
-               if (dados.bairro != '') {
+               if (dados.bairro != "") {
                   district.val(dados.bairro);
                   district.prop("disabled", true);
                }
-               if (dados.localidade != '') {
+               if (dados.localidade != "") {
                   city.val(dados.localidade);
                   city.prop("disabled", true);
                }
-               if (dados.uf != '') {
+               if (dados.uf != "") {
                   state.val(dados.uf);
                   state.prop("disabled", true);
                }
@@ -51,46 +51,42 @@ const InitCadastroEvento = () => {
    btnCadastrar.click(() => {
       if (form[0].checkValidity()) {
          var dadosCadastro = {
-            "name": name.val(),
-            "eventType": eventType.val(),
-            "date": date.val(),
-            "CEP": CEP.val(),
-            "street": street.val(),
-            "number": number.val(),
-            "district": district.val(),
-            "city": city.val(),
-            "state": state.val(),
-            "complement": complement.val(),
-            "peopleQnt": peopleQnt.val(),
-            "artistValue": artistValue.val()
+            name: name.val(),
+            eventType: eventType.val(),
+            date: date.val(),
+            CEP: CEP.val(),
+            street: street.val(),
+            number: number.val(),
+            district: district.val(),
+            city: city.val(),
+            state: state.val(),
+            complement: complement.val(),
+            peopleQnt: peopleQnt.val(),
+            artistValue: artistValue.val(),
          };
 
-         console.log(dadosCadastro)
+         console.log(dadosCadastro);
 
          $.ajax({
-
             url: "http://localhost/json/teste.php",
             type: "POST",
             data: dadosCadastro,
             dataType: "json",
-            beforeSend: function () {
-               alert("carregando...")
-            }
-
-         }).done(function () {
-            alert("Enviado");
+            beforeSend: function() {
+               alert("carregando...");
+            },
          })
-            .fail(function () {
+            .done(function() {
+               alert("Enviado");
+            })
+            .fail(function() {
                alert("error");
             })
-            .always(function () {
-
+            .always(function() {
                alert("complete");
-
             });
-      };
+      }
    });
-
 };
 
 InitCadastroEvento();
