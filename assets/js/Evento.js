2cb342e23c1c150382c7dcc0ccf37e95c87c6cfa
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2019-11-07 18:49:12 -0300

    mudança nas requisições

--- assets/js/Evento.js
+++ assets/js/Evento.js
@@ -1,38 +1,28 @@
-function getEvento(id) {
-   var http = new XMLHttpRequest();
-   var url = `http://localhost:8080/ashow/evento/${id}`;
-
-   http.open('GET', url, true);
-   http.setRequestHeader('Content-type', 'application/json');
-   http.onreadystatechange = function () {
-      if (http.readyState == 4 && http.status == 200) {
-         dados = (JSON.parse(http.responseText));
-
-         let data = dados.data;
-         data = data[8] + data[9] + "/" + data[5] + data[6] + "/" + data[0] + data[1] + data[2] + data[3];
-         let hora = dados.data[11] + dados.data[12] + dados.data[13] + dados.data[14] + dados.data[15];
-         let htmlTexto = ``;
-         if (dados.open == true) {
-            htmlTexto += `
-            <h2 class="titleArtista">${dados.nome}</h2>
-            <div class="dadosEvento" id="dadosEvento">
-               <h3>Estilo: ${dados.estilo}</h3>
-               <h3>Data: ${data}</h3>
-               <h3>Hora: ${hora}</h3>
-               <h3>Endereço: </h3>
-               <h3>${dados.endereco.rua}, ${dados.endereco.numero}, ${dados.endereco.complemento},
-                  ${dados.endereco.bairro}, ${dados.endereco.cidade}, ${dados.endereco.uf} </h3>
-               <h3>Quantidade de artistas: ${dados.quantidadeArtistas}</h3>
-               <h3>Valor base: ${dados.valor}</h3>
-            </div>`;
-         }
-         document.getElementById("evento").innerHTML = htmlTexto;
-      }
-   };
-   http.send();
-}
-
-
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
-getEvento(id);
+
+
+(async () => {
+   const dados = await getEvento(id);
+
+   let data = dados.data;
+   data = data[8] + data[9] + "/" + data[5] + data[6] + "/" + data[0] + data[1] + data[2] + data[3];
+   let hora = dados.data[11] + dados.data[12] + dados.data[13] + dados.data[14] + dados.data[15];
+   let htmlTexto = ``;
+   if (dados.open == true) {
+      htmlTexto += `
+      <h2 class="titleArtista">${dados.nome}</h2>
+      <div class="dadosEvento" id="dadosEvento">
+         <h3>Estilo: ${dados.estilo}</h3>
+         <h3>Data: ${data}</h3>
+         <h3>Hora: ${hora}</h3>
+         <h3>Endereço: </h3>
+         <h3>${dados.endereco.rua}, ${dados.endereco.numero}, ${dados.endereco.complemento},
+            ${dados.endereco.bairro}, ${dados.endereco.cidade}, ${dados.endereco.uf} </h3>
+         <h3>Quantidade de artistas: ${dados.quantidadeArtistas}</h3>
+         <h3>Valor base: ${dados.valor}</h3>
+      </div>`;
+   }
+   document.getElementById("evento").innerHTML = htmlTexto;
+})()
+
--- assets/js/RequisicoesEvento.js
+++ assets/js/RequisicoesEvento.js
@@ -1,98 +1,16 @@
-const AddEvento = () => {
-   const form = $('#formEvent');
-   const name = $('#name');
-   const eventType = $('#eventType');
-   const date = $('#date');
-   const hours = $('#hours');
-   const CEP = $('#CEP');
-   const street = $('#street');
-   const number = $('#number');
-   const district = $('#district');
-   const city = $('#city');
-   const state = $('#state');
-   const complement = $('#complement');
-   const peopleQnt = $('#peopleQnt');
-   const artistValue = $('#artistValue');
-   const artistQnt = $('#artistQnt');
-   const btnCadastrar = $('#cadastrarEventoBtn');
+function AddEvento(dadosCadastro) {
+   var http = new XMLHttpRequest();
+   var url = 'http://localhost:8080/ashow/evento/add';
 
-   CEP.change(() => {
-      if (CEP.val().length >= 8) {
-         $.getJSON('https://viacep.com.br/ws/' + CEP.val() + '/json/', function (
-            dados,
-         ) {
-            street.prop('disabled', false);
-            district.prop('disabled', false);
-            city.prop('disabled', false);
-            state.prop('disabled', false);
-            if (!('erro' in dados)) {
-               if (dados.logradouro != '') {
-                  street.val(dados.logradouro);
-                  street.prop('disabled', true);
-               }
-               if (dados.bairro != '') {
-                  district.val(dados.bairro);
-                  district.prop('disabled', true);
-               }
-               if (dados.localidade != '') {
-                  city.val(dados.localidade);
-                  city.prop('disabled', true);
-               }
-               if (dados.uf != '') {
-                  state.val(dados.uf);
-                  state.prop('disabled', true);
-               }
-            }
-         });
-      } else {
-         street.prop('disabled', false);
-         district.prop('disabled', false);
-         city.prop('disabled', false);
-         state.prop('disabled', false);
+   http.open('POST', url, true);
+   http.setRequestHeader('Content-type', 'application/json');
+   http.onreadystatechange = function () {
+      if (http.readyState == 4 && http.status == 200) {
+         // console.log(http.responseText);
       }
-   });
-
-   btnCadastrar.click(() => {
-      if (form[0].checkValidity()) {
-         var dadosCadastro = {
-            capacidadeEsperada: peopleQnt.val(),
-            data: date.val() + "T" + hours.val(),
-            endereco: {
-               bairro: district.val(),
-               cep: CEP.val(),
-               cidade: city.val(),
-               complemento: complement.val(),
-               numero: number.val(),
-               rua: street.val(),
-               uf: state.val()
-            },
-            estilo: eventType.val(),
-            nome: name.val(),
-            open: true,
-            quantidadeArtistas: artistQnt.val(),
-            valor: artistValue.val()
-         }
-
-         var http = new XMLHttpRequest();
-         var url = 'http://localhost:8080/ashow/evento/add';
-
-         http.open('POST', url, true);
-         http.setRequestHeader('Content-type', 'application/json');
-         http.onreadystatechange = function () {
-            if (http.readyState == 4 && http.status == 200) {
-               console.log(http.responseText);
-               if (http.responseText == 'true') {
-                  alert("Evento cadastrado");
-                  window.location.href = ("./home.html");
-               } else {
-                  alert("Erro ao cadastrar o evento")
-               }
-            }
-         };
-         http.send(JSON.stringify(dadosCadastro));
-      }
-   });
-};
+   };
+   http.send(JSON.stringify(dadosCadastro));
+}
 
 function getAllEventos() {
    var http = new XMLHttpRequest();
@@ -108,19 +26,7 @@ function getAllEventos() {
    http.send(JSON.stringify());
 }
 
-function getEvento(id) {
-   var http = new XMLHttpRequest();
-   var url = `http://localhost:8080/ashow/evento/${id}`;
-
-   http.open('GET', url, true);
-   http.setRequestHeader('Content-type', 'application/json');
-   http.onreadystatechange = function () {
-      if (http.readyState == 4 && http.status == 200) {
-         console.log(JSON.parse(http.responseText));
-      }
-   };
-   http.send(JSON.stringify());
-}
+const getEvento = async id => await (await fetch(`http://localhost:8080/ashow/evento/${id}`)).json();
 
 function deleteEvento(id) {
    var http = new XMLHttpRequest();
@@ -135,5 +41,3 @@ function deleteEvento(id) {
    };
    http.send(JSON.stringify());
 }
-
-AddEvento();
