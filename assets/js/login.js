5821378e7a95dafe734f3b9bd8fc0303a592e420
Guilherme <guilhergoa@hotmail.com>
2019-11-07 11:49:29 -0300

    login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,27 +1,63 @@
-{
-   /* <script>
+const formLog = $('#formLog');
 const emailLog = $('#emailLog');
 const senhaLog = $('#senhaLog');
-const tryLog = $('#tryLog');
+const tryLogArtista = $('#tryLog-artista');
+const tryLogContratante = $('#tryLog-contratante');
 
-tryLog.click(() => {
-   var dadosTry = senhaLog.val();
+tryLogArtista.click(() => {
+   if (formLog[0].checkValidity()) {
+      var dadosTry = {
+         type: 'loginUsuario',
+         email: emailLog.val(),
+         senha: senhaLog.val(),
+      };
 
-   var http = new XMLHttpRequest();
-   var url =
-      'http://localhost:8080/ashow/artista/' + emailLog.val();
-   var resposta;
+      var http = new XMLHttpRequest();
+      var url = 'http://localhost:8080/ashow/artista/log';
+      var resposta;
 
-   http.open('POST', url, true);
-   http.setRequestHeader('Content-type', 'text/plain');
-   http.onreadystatechange = function() {
-      if (http.readyState == 4 && http.status == 200) {
-         resposta = http.responseText;
-         window.location.href =
-            './asset/pages/home.html' + emailLog.val();
-      }
-   };
-   http.send(dadosTry);
+      http.open('POST', url, true);
+      http.setRequestHeader('Content-type', 'application/json');
+      http.onreadystatechange = function() {
+         if (http.readyState == 4 && http.status == 200) {
+            console.log(http.responseText);
+            if (http.responseText == 'true') {
+               sessionStorage.setItem('emaillog', dadosTry.email);
+               window.location.href = '../../pages/home.html';
+            } else {
+               alert('Senha ou email errado');
+            }
+         }
+      };
+      http.send(JSON.stringify(dadosTry));
+   }
 });
-</script> */
-}
+
+tryLogContratante.click(() => {
+   if (formLog[0].checkValidity()) {
+      var dadosTry = {
+         type: 'loginUsuario',
+         email: emailLog.val(),
+         senha: senhaLog.val(),
+      };
+
+      var http = new XMLHttpRequest();
+      var url = 'http://localhost:8080/ashow/contratante/log';
+      var resposta;
+
+      http.open('POST', url, true);
+      http.setRequestHeader('Content-type', 'application/json');
+      http.onreadystatechange = function() {
+         if (http.readyState == 4 && http.status == 200) {
+            console.log(http.responseText);
+            if (http.responseText == 'true') {
+               sessionStorage.setItem('emailLog', dadosTry.email);
+               window.location.href = '../../pages/home.html';
+            } else {
+               alert('Senha ou email errado');
+            }
+         }
+      };
+      http.send(JSON.stringify(dadosTry));
+   }
+});
