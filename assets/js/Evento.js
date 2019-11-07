25e0903c96f697899f065b8775b67e30f40295ae
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2019-11-07 18:01:26 -0300

    Evento mostrando dados

--- assets/js/Evento.js
+++ assets/js/Evento.js
@@ -6,16 +6,27 @@ function getEvento(id) {
    http.setRequestHeader('Content-type', 'application/json');
    http.onreadystatechange = function () {
       if (http.readyState == 4 && http.status == 200) {
-         data = (JSON.parse(http.responseText));
+         dados = (JSON.parse(http.responseText));
 
-         console.log(data.open)
-         let dados = ``;
-         if (data.open == true) {
-            dados += `
-               <h2 class="titleArtista">${data.nome}</h2>
-               <div class="card" id="card"></div>`;
+         let data = dados.data;
+         data = data[8] + data[9] + "/" + data[5] + data[6] + "/" + data[0] + data[1] + data[2] + data[3];
+         let hora = dados.data[11] + dados.data[12] + dados.data[13] + dados.data[14] + dados.data[15];
+         let htmlTexto = ``;
+         if (dados.open == true) {
+            htmlTexto += `
+            <h2 class="titleArtista">${dados.nome}</h2>
+            <div class="dadosEvento" id="dadosEvento">
+               <h3>Estilo: ${dados.estilo}</h3>
+               <h3>Data: ${data}</h3>
+               <h3>Hora: ${hora}</h3>
+               <h3>Endereço: </h3>
+               <h3>${dados.endereco.rua}, ${dados.endereco.numero}, ${dados.endereco.complemento},
+                  ${dados.endereco.bairro}, ${dados.endereco.cidade}, ${dados.endereco.uf} </h3>
+               <h3>Quantidade de artistas: ${dados.quantidadeArtistas}</h3>
+               <h3>Valor base: ${dados.valor}</h3>
+            </div>`;
          }
-         document.getElementById("evento").innerHTML = dados;
+         document.getElementById("evento").innerHTML = htmlTexto;
       }
    };
    http.send();
