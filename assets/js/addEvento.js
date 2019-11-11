4c5b7f6ce1d5bd4061f6e607ae966a39d07ef5ff
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2019-11-11 00:27:47 -0300

    fix

--- assets/js/addEvento.js
+++ assets/js/addEvento.js
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 const form = $('#formEvent');
 const name = $('#name');
 const eventType = $('#eventType');
@@ -21,6 +22,37 @@ CEP.change(() => {
       $.getJSON('https://viacep.com.br/ws/' + CEP.val() + '/json/', function (
          dados,
       ) {
+=======
+   CEP.change(() => {
+      if (CEP.val().length >= 8) {
+         $.getJSON('https://viacep.com.br/ws/' + CEP.val() + '/json/', function(
+            dados,
+         ) {
+            street.prop('disabled', false);
+            district.prop('disabled', false);
+            city.prop('disabled', false);
+            state.prop('disabled', false);
+            if (!('erro' in dados)) {
+               if (dados.logradouro != '') {
+                  street.val(dados.logradouro);
+                  street.prop('disabled', true);
+               }
+               if (dados.bairro != '') {
+                  district.val(dados.bairro);
+                  district.prop('disabled', true);
+               }
+               if (dados.localidade != '') {
+                  city.val(dados.localidade);
+                  city.prop('disabled', true);
+               }
+               if (dados.uf != '') {
+                  state.val(dados.uf);
+                  state.prop('disabled', true);
+               }
+            }
+         });
+      } else {
+>>>>>>> cssCadastroEvento
          street.prop('disabled', false);
          district.prop('disabled', false);
          city.prop('disabled', false);
@@ -52,14 +84,19 @@ CEP.change(() => {
    }
 });
 
+<<<<<<< HEAD
 form.on("submit", (event) => {
    event.preventDefault();
+=======
+   form.on('submit', event => {
+      event.preventDefault();
+>>>>>>> cssCadastroEvento
 
    if (form[0].checkValidity()) {
       (async () => {
          var dadosCadastro = {
             capacidadeEsperada: peopleQnt.val(),
-            data: date.val() + "T" + hours.val(),
+            data: date.val() + 'T' + hours.val(),
             endereco: {
                bairro: district.val(),
                cep: CEP.val(),
@@ -67,15 +104,17 @@ form.on("submit", (event) => {
                complemento: complement.val(),
                numero: number.val(),
                rua: street.val(),
-               uf: state.val()
+               uf: state.val(),
             },
             estilo: eventType.val(),
             nome: name.val(),
             open: true,
             quantidadeArtistas: artistQnt.val(),
-            valor: artistValue.val()
-         }
+            valor: artistValue.val(),
+         };
+         console.log(dadosCadastro.quantidadeArtistas);
 
+<<<<<<< HEAD
          const resposta = await addPostEvento(dadosCadastro);
          if (resposta == 'true') {
             alert("Evento cadastrado");
@@ -88,4 +127,26 @@ form.on("submit", (event) => {
 
 })
 
+=======
+         var http = new XMLHttpRequest();
+         var url = 'http://localhost:8080/ashow/evento/add';
+
+         http.open('POST', url, true);
+         http.setRequestHeader('Content-type', 'application/json');
+         http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+               // console.log(http.responseText);
+               if (http.responseText == 'true') {
+                  alert('Evento cadastrado');
+                  window.location.href = './home.html';
+               } else {
+                  alert('Erro ao cadastrar o evento');
+               }
+            }
+         };
+         http.send(JSON.stringify(dadosCadastro));
+      }
+   });
+};
+>>>>>>> cssCadastroEvento
 
