1dfaf8445879dd54851e7aa51054d13e1db11773
MatheusFM <matheusfelipeferreiramartins@gmail.com>
2019-12-01 00:01:02 -0300

    cards

--- assets/js/mostraTodosEventosHome.js
+++ assets/js/mostraTodosEventosHome.js
@@ -1,48 +1,50 @@
 const mostraTodosNaHome = () => {
-   var http = new XMLHttpRequest();
-   var url = 'http://localhost:8080/ashow/evento/all';
+  var http = new XMLHttpRequest();
+  var url = "http://localhost:8080/ashow/evento/all";
 
-   http.open('GET', url, true);
-   http.setRequestHeader('Content-type', 'application/json');
-   http.onreadystatechange = function() {
-      if (http.readyState == 4 && http.status == 200) {
-         var dados = http.responseText;
+  http.open("GET", url, true);
+  http.setRequestHeader("Content-type", "application/json");
+  http.onreadystatechange = function() {
+    if (http.readyState == 4 && http.status == 200) {
+      var dados = http.responseText;
 
-         dados = JSON.parse(dados);
+      dados = JSON.parse(dados);
 
-         if (dados.length == 0) {
-            let na = document.getElementById('void');
-            na.style.display = 'block';
-            let acabou = document.getElementById('NA');
-            acabou.style.display = 'block';
-            document.getElementById('titleHome').style.display = 'none';
-         } else if (dados.length <= 6) {
-            let na = document.getElementById('void');
-            na.style.display = 'block';
-            let acabou = document.getElementById('NA');
-            acabou.style.display = 'none';
-            document.getElementById('titleHome').style.display = 'block';
-         } else {
-            let na = document.getElementById('void');
-            na.style.display = 'none';
-            document.getElementById('titleHome').style.display = 'block';
-         }
+      if (dados.length == 0) {
+        let na = document.getElementById("void");
+        na.style.display = "block";
+        let acabou = document.getElementById("NA");
+        acabou.style.display = "block";
+        document.getElementById("titleHome").style.display = "none";
+      } else if (dados.length <= 4) {
+        let na = document.getElementById("void");
+        na.style.display = "block";
+        let acabou = document.getElementById("NA");
+        acabou.style.display = "none";
+        document.getElementById("titleHome").style.display = "block";
+      } else {
+        let na = document.getElementById("void");
+        na.style.display = "none";
+        document.getElementById("titleHome").style.display = "block";
+      }
 
-         var resp = ``;
-         for (var i = dados.length - 1; i >= 0; i--) {
-            let data = dados[i].data;
-            var dia =
-               data[8] +
-               data[9] +
-               '/' +
-               data[5] +
-               data[6] +
-               '/' +
-               data[0] +
-               data[1] +
-               data[2] +
-               data[3];
-            resp += `<a id="cardEventos" class="cardEventos" href="./evento.html?idEvt=${dados[i].id}"><div id="card-${dados[i].id}">
+      var resp = ``;
+      for (var i = dados.length - 1; i >= 0; i--) {
+        let data = dados[i].data;
+        var dia =
+          data[8] +
+          data[9] +
+          "/" +
+          data[5] +
+          data[6] +
+          "/" +
+          data[0] +
+          data[1] +
+          data[2] +
+          data[3];
+        resp += `<a id="cardEventos" class="cardEventos" href="./evento.html?idEvt=${dados[i].id}">
+
+            <div id="card-${dados[i].id}">
       <h2 id="NomeArtistico-${dados[i].id}" class = "nomeEvento">${dados[i].nome}</h2>
       <h2 id="Nome-${dados[i].id}">${dia}</h2>
       <h2 id="Estilo-${dados[i].id}">
@@ -54,12 +56,12 @@ const mostraTodosNaHome = () => {
          <span class="value">${dados[i].endereco.bairro}, ${dados[i].endereco.cidade}</span>
       </h3>
     </div></a>`;
-         }
-         document.getElementById('card').innerHTML = resp;
-         // $('#card').html(resp);
       }
-   };
-   http.send();
-}
+      document.getElementById("card").innerHTML = resp;
+      // $('#card').html(resp);
+    }
+  };
+  http.send();
+};
 
 mostraTodosNaHome();
